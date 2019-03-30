20aeeb7349c136e8057fe5f22bb57a85b47bbec3
manhvu <manh.vv.htth@gmail.com>
2019-03-30 20:56:17 +0700

    February updated

---
+++ excel_tool/MonthEnum.py
@@ -0,0 +1,16 @@
+from enum import Enum
+
+
+class MonthEnum(Enum):
+    Jan = 1
+    Feb = 2
+    Mar = 3
+    Apr = 4
+    May = 5
+    Jun = 6
+    Jul = 7
+    Aug = 8
+    Sep = 9
+    Oct = 10
+    Nov = 11
+    Dec = 12
