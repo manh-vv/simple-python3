7ff831e31823db07da66b81bea5dabb14ed019c1
manhvu <manh.vv.htth@gmail.com>
2019-03-30 19:26:04 +0700

    January 2019 updated

--- excel_tool/variables.py
+++ excel_tool/variables.py
@@ -8,7 +8,7 @@ template_description = {
     'template_sheet_name': 'customer_name',
     'sheet_title_col': 'A1',
     'inventory_num_col': 'B2',
-    'inventory_num_prefix': '1218HCM-',  # tobe updated
+    'inventory_num_prefix': '0119HCM-',  # tobe updated
     'percent_col': 'G46',
     'row_start': 5,
     'available_rows': 40,
@@ -17,31 +17,31 @@ template_description = {
 # Input
 data_input = {
     'import_new': False,
-    'data_file_name': 'data_book_whole_year.xlsx',
-    'data_range': ('A', 3, 24383),
+    'data_file_name': 'data_book2.xlsx',
+    'data_range': ('A', 2, 4311),
     'sheet_name': 'Sheet1',
     'data_col': {
         'month': 'A',
         'Region': 'B',
         'customer_code': 'C',
         'customer_name': 'D',
-        'sup_name': 'N',
-        'net_sale': 'F',
-        'vat_sale': 'H',
-        'District': 'E',
-        'Date': 'G',
+        'sup_name': 'T',
+        'net_sale': 'Q',
+        'vat_sale': 'R',
+        'District': 'F',
+        'Date': 'S',
     },
-    'percent_file_name': 'phan_tram_12.xlsx',
+    'percent_file_name': 'phan_tram_1.xlsx',
     'working_months': [
         ('dec', 'H')
     ],
     'percent_file_des': {
         'row_data_start': 2,
-        'row_data_end': 67,
+        'row_data_end': 81,
         'col_sup_name': 'E',
         'col_customer_code': 'B',
     },
-    'current_month': 'Dec',
-    'enable_export_incentive': True,
-    'quarter_month_start': 10
+    'current_month': 'Jan',
+    'enable_export_incentive': False,
+    'quarter_month_start': 1
 }
